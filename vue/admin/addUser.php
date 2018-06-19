237cdf8287c69fd76c1e1a70d7f666dd02113b37
ibnibrahim <aliouibnibrahim@gmail.com>
2018-06-19 12:17:09 +0000

    Script for adding user

---
+++ vue/admin/addUser.php
@@ -0,0 +1,18 @@
+<form action="" method="POST">
+    <label for="nom">Nom : </label>
+    <input type="text" id="nom" name="nom"> </br></br>
+    <label for="prenom">Prénom : </label>
+    <input type="text" id="prenom" name="prenom" > </br></br>
+    <label for="mail">Email : </label>
+    <input type="text" id="mail" name="mail"> </br></br>
+    <label for="password">Mot de passe : </label>
+    <input type="password" id="password" name="password"> </br></br>
+    <label for="passwordConf">Mot de passe de confirmation : </label>
+    <input type="password" id="passwordConf" name="passwordConf"> </br></br>
+    <label for="">Séléctionner le type de compte : </label>
+    <select>
+        <option value="administrateur">Administrateur</option>
+        <option value="responsable_pedagogique">Responsable Pédagogique</option>
+        <option value="enseignant">Enseignant</option>
+    </select>
+</form>
