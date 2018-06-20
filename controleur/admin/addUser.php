5e4a81712b7cefed81af7ddb5bb1bc84f4d03c09
ibnibrahim <aliouibnibrahim@gmail.com>
2018-06-20 11:42:57 +0000

    AddUser Controller Script

--- controleur/admin/addUser.php
+++ controleur/admin/addUser.php
@@ -1,7 +1,12 @@
 <?php
 
-    function addUser() {
-        
+    if(isset($_POST['addUser'])){
+        if(isset($_POST['prenom'],$_POST['nom'],$_POST['mail'],$_POST['date_expiration'],$_POST['accountType'])){
+            $mail = secure($_POST['mail']);
+            if(!isEmail($mail)) {
+                echo 'Veuillez vérifier l\'adresse mail saisie';
+            }
+        }
     }
 
 ?>
