32e0cd4b47333b3892f144b95a4f59c04b241d84
ibnibrahim <aliouibnibrahim@gmail.com>
2018-07-16 14:25:11 +0000

    Only checked checkboxes

--- vue/admin/showSubjects.php
+++ vue/admin/showSubjects.php
@@ -9,8 +9,7 @@
       <div class="modal-body">
         <form action="" method="POST">
             <?php for($i=0; $i<count($ecWithoutGroup); $i++) { ?>
-                <input type="checkbox" id="subject" name="subject"><label for="subject"> <?php print_r($ecWithoutGroup[$i]['ec_nom']); ?></label> </br></br>
-                <input type="hidden" name="ecId[]" value="<?php echo ($ecWithoutGroup[$i]["ec_id"]); ?>">
+                <input type="checkbox" name="ecId[]" value="<?php echo ($ecWithoutGroup[$i]["ec_id"]); ?>" id="subject<?php echo ($ecWithoutGroup[$i]["ec_id"]); ?>" ><label for="subject<?php echo ($ecWithoutGroup[$i]["ec_id"]); ?>"> <?php print_r($ecWithoutGroup[$i]['ec_nom']); ?></label> </br></br>
             <?php } ?>
             <input type="hidden" name="groupId" value="<?php echo($groupId); ?>">
             <center>
@@ -44,6 +43,106 @@
                 </div>
             <center>
                 <input class="btn btn-danger" type="submit" value="Supprimer" name="affecter">
+            </center>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">FERMER</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="suggestionNom" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+          <h4 style="float:left" class="modal-title" id="">Sélectionner un nom</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+      </div>
+      <div class="modal-body">
+      <?php $PROJET=new Projet; ?>
+        <form action="" method="POST">
+            <?php for ($i=0; $i<count($suggestions); $i++) {
+                if($suggestions[$i]['suggestion_cible']!='ec_nom') continue;    
+            ?>
+                <input type="radio" id="suggestionNom" name="suggestionNom" value="<?php echo($suggestions[$i]['suggestion_valeur']); ?>"><label for="subject"><?php print_r($suggestions[$i]['suggestion_valeur']); ?></label> </br></br>
+                <input type="hidden" name="ecId" value="">
+            <?php } ?>
+            <input type="hidden" name="groupId" value="<?php echo($groupId); ?>">
+            <div>
+                <textarea name="" id="" cols="60" rows="10">
+                    <?php for ($i=0; $i<count($ecInGroup); $i++) { print_r($ecInGroup[$i]['ec_nom']); } ?>
+                </textarea>
+            </div>
+            <center>
+                <input class="btn btn-success" type="submit" value="Appliquer" name="appliquerNom">
+            </center>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">FERMER</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="suggestionCompetence" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+          <h4 style="float:left" class="modal-title" id="">Sélectionner une compétence</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+      </div>
+      <div class="modal-body">
+      <?php $PROJET=new Projet; ?>
+        <form action="" method="POST">
+                <?php for ($i=0; $i<count($suggestions); $i++) {
+                    if($suggestions[$i]['suggestion_cible']!=='ec_competence') continue;    
+                ?>
+                    <input type="radio" id="suggestionCompet" name="suggestionCompet" value="<?php echo($suggestions[$i]['suggestion_valeur']); ?>"><label for="subject"><?php print_r($suggestions[$i]['suggestion_valeur']); ?></label> </br></br>
+                    <input type="hidden" name="userId[]" value="">
+                <?php } ?>
+                <input type="hidden" name="groupId" value="<?php echo($groupId); ?>">
+                <div>
+                    <textarea name="" id="" cols="60" rows="10">
+                        <?php for ($i=0; $i<count($ecInGroup); $i++) { print_r($ecInGroup[$i]['ec_competence']); } ?>
+                    </textarea>
+                </div>
+            <center>
+                <input class="btn btn-success" type="submit" value="Appliquer" name="appliquerCompet">
+            </center>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">FERMER</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="suggestionPrerequis" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+          <h4 style="float:left" class="modal-title" id="">Sélectionner un prérequis</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+      </div>
+      <div class="modal-body">
+      <?php $PROJET=new Projet; ?>
+        <form action="" method="POST">
+                <?php for ($i=0; $i<count($suggestions); $i++) {
+                    if($suggestions[$i]['suggestion_cible']!='ec_prerequis') continue;
+                ?>
+                
+                    <input type="radio" id="suggestionPrerequis" name="suggestionPrerequis" value="<?php echo($suggestions[$i]['suggestion_valeur']); ?>"><label for="subject"><?php print_r($suggestions[$i]['suggestion_valeur']); ?></label> </br></br>
+                    <input type="hidden" name="userId[]" value="">
+                <?php } ?>
+                <input type="hidden" name="groupId" value="<?php echo($groupId); ?>">
+                <div>
+                    <textarea name="" id="" cols="60" rows="10">
+                        <?php for ($i=0; $i<count($ecInGroup); $i++) { print_r($ecInGroup[$i]['ec_prerequis']); } ?>
+                    </textarea>
+                </div>
+            <center>
+                <input class="btn btn-success" type="submit" value="Appliquer" name="appliquerPrerequis">
             </center>
         </form>
       </div>
@@ -106,9 +205,21 @@
                                     ?>
                                     <tr>
                                         <td><?php print_r($ecInGroup[$i]['ec_code']); ?></td>
-                                        <td><?php print_r($ecInGroup[$i]['ec_nom']); ?></td>
-                                        <td><?php print_r($ecInGroup[$i]['ec_competence']); ?></td>
-                                        <td><?php print_r($ecInGroup[$i]['ec_prerequis']); ?></td>
+                                        <td>
+                                            <a class="" data-toggle="modal" href="#suggestionNom">
+                                            <?php print_r($ecInGroup[$i]['ec_nom']); ?>
+                                            </a>
+                                        </td>
+                                        <td>
+                                            <a class="" data-toggle="modal" href="#suggestionCompetence">
+                                            <?php print_r($ecInGroup[$i]['ec_competence']); ?>
+                                            </a>
+                                        </td>
+                                        <td>
+                                            <a class="" data-toggle="modal" href="#suggestionPrerequis">
+                                            <?php print_r($ecInGroup[$i]['ec_prerequis']); ?>
+                                            </a>
+                                        </td>
                                     </tr>
                                     <?php } ?>
                                 </tbody>
