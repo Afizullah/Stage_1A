7d99eeb5ed986291a5bd3f2bc4ca54c66a633784
niakhos <niakhdaouda@gmail.com>
2018-07-18 09:40:59 +0000

    Notifications admin & correctif sur application suggestion

--- model/admin/assync.loadSuggest.php
+++ model/admin/assync.loadSuggest.php
@@ -1,7 +1,24 @@
 <?php
     class Sugges extends DB{
             public static function getAll($projetId,$realCible,$idCible){
-                return DB::getData("suggestions_projet_utilisateur NATURAL JOIN suggestions NATURAL JOIN utilisateurs NATURAL JOIN projet","suggestion_id,user_nom,user_prenom,user_mail,suggestion_valeur",[["projet_id",intval($projetId)],["suggestion_etat","en cours"],["suggestion_cible",$realCible],["suggestion_cible_id",$idCible]]);
+                if($result = DB::getData("suggestions_projet_utilisateur NATURAL JOIN suggestions NATURAL JOIN utilisateurs NATURAL JOIN projet","suggestion_id,user_nom,user_prenom,user_mail,suggestion_valeur",[["projet_id",intval($projetId)],["suggestion_etat","en cours"],["suggestion_cible",$realCible],["suggestion_cible_id",$idCible]])){
+                    foreach ($result as $resultKey => $value) {
+                        self::checkReadNotif($value["suggestion_id"]);
+                    }
+                }
+                return $result;
+            }
+            public static function getNotRead($projetId){
+                return DB::getData("suggestions_projet_utilisateur NATURAL JOIN suggestions NATURAL JOIN utilisateurs NATURAL JOIN projet","suggestion_id,suggestion_cible,user_nom,user_prenom,user_mail,suggestion_valeur",[["projet_id",intval($projetId)],["suggestion_etat","en cours"],["suggestion_vue",0]]);
+            }
+            public static function checkReadNotif($suggestId){
+                return DB::update("suggestions",[["suggestion_vue",1]],[["suggestion_id",intval($suggestId)]]);
+            }
+            public static function changeStatusSuggestion($suggestionId,$newStatus){
+                return DB::update("suggestions",[["suggestion_etat",$newStatus]],[["suggestion_id",intval($suggestionId)]]);
+            }
+            public static function getSugges($suggestId){
+                return DB::getLine("suggestions_projet_utilisateur NATURAL JOIN suggestions NATURAL JOIN utilisateurs NATURAL JOIN projet","suggestion_id,suggestion_cible,user_nom,user_prenom,user_mail,suggestion_valeur",[["suggestion_id",intval($suggestId)]]);
             }
             public static function apply($suggestionId){
                 if($suggestion = DB::getLine("suggestions","*",[["suggestion_id",intval($suggestionId)]])){
@@ -14,6 +31,7 @@
                         if($elementToModify = DB::getLine($table,"*",[[$idName,$elementId]])){
                             $newValue = $suggestion["suggestion_valeur"];
                             if(DB::update($table,[[$attrib,$newValue]],[[$idName,$elementId]])){
+                                self::changeStatusSuggestion($suggestionId,"applique");
                                 return true;
                             }
                         }
---
+++ vue/admin/showSuggestion.php
@@ -0,0 +1,20 @@
+<div style="background-color:white;min-height:450px" class="col-lg-12">
+    <div  style="border:1px solid rgba(200,200,200,0.5);margin-bottom:10px" class="col-md-12">
+        <div style="border:1px solid rgba(200,200,200,0.5);margin-left:-15px;padding-left:0px;padding-top:5px" class="col-md-3">
+            <center title="<?php echo 'email'; ?>">
+                <i style="border:1px solid black;;padding:10px;height:60px;border-radius:60px;width:60px" class="fa fa-3x fa-user"></i><br />
+                <?php
+                echo " Daouda Niakh ";
+                ?><br />
+                <button class="btn btn-sm btn-success" type="button" name="button">Appliquer</button>
+                <button class="btn btn-sm btn-warning" type="button" name="button">Rejeter</button>
+                <br />
+            </center>
+        </div>
+        <div   class="col-md-9">
+            <?php
+            echo "test";
+            ?>
+        </div>
+    </div>
+</div>
